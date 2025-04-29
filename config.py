e9e3e9cbe79336c263bb7b83e5fdcb6f9f41ce20
rsrawatofficial <rsrasasingh@gmail.com>
2025-04-29 23:26:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-"""
 from os import getenv
 
 
@@ -11,34 +10,3 @@ MONGO_URL = getenv("MONGO_DB", "mongodb+srv://rsrasasingh:FS2G9YbI28KbPHLC@clust
 
 CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002566364060"))
 PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002566364060"))
-
-"""
-#
-
-
-
-
-# --------------M----------------------------------
-
-import os
-from os import getenv
-# ---------------R---------------------------------
-API_ID = int(os.environ.get("API_ID"))
-# ------------------------------------------------
-API_HASH = os.environ.get("API_HASH")
-# ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-# -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME")
-# ------------------X------------------------------
-OWNER_ID = int(os.environ.get("7804396225"))
-# ------------------X------------------------------
-
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-# ------------------------------------------------
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID"))
-# ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL")
-# -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS"))
-
